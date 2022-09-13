64a331f04d76350cabb01de1e13686f5390d4a83
romantafelski <romantafelski18@gmail.com>
2022-09-13 12:54:23 -0400

    Adding more to the dev branch to catch up to derrick

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import axios from 'axios';
+import {useState, useEffect}  from 'react';
 
 const App = () => {
   
   return (
-    <h1>Header for react</h1>
+    <h1>Header</h1>
   )
 }
 export default App;
