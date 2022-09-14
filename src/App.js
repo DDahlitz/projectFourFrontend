93dd3ee7ca363bfd4251688bb0dedebd63fe45ea
Derrick Dahlitz <ddahlitz@iMac-28.lan>
2022-09-14 07:34:56 -0700

    adding routes and basic return to the app.js file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css'
-import {useState, useEffect} from 'react'
+import {useState, useEffect, isValidElement} from 'react'
 import React from 'react'
 import axios from 'axios'
 
@@ -19,27 +19,56 @@ const getProducts = () => {
   ).catch((error) => console.error(error))
 }
 
+const handleCreate = (addItem) => {
+  let nextId = products[products.length - 1].id + 1
+  axios.post('http://localhost:8000/api/products', addItem)
+    .then((response) => {
+      addItem.id = nextId
+      setProducts([...products, response.data])
+    })
+}
+
+const handleDelete = (deletedItem) => {
+  axios.delete('http://localhost:8000/api/products/' + deletedItem.id)
+  .then ((response) => {
+    setProducts (products.filter( item => item.id !== deletedItem.id))
+  })
+}
+
+const handleUpdate = (editItem) => {
+  axios.delete('http://localhost:8000/api/products/' + editItem.id, editItem)
+  .then((response) => {
+    setProducts(products.map((item) => {
+      return isValidElement.id !== response.data.id ? item : editItem
+    }))
+  })
+}
+
 
 
 useEffect(() => {
   getProducts()
-}, [])
+}, []) 
 
   return (
     <>
     <h1>Header</h1>
-    {products.map((product) => {
+    <New handleCreate={handleCreate} />
+    <div>
+    {products.map((item) => {
       return (
-        <div key = {product.id}>
-          <img src = {product.image} />
-          <h4> Name: {product.name}</h4>
-          <h6> Description: {product.description} </h6>
-          <h4> Price: {product.price} </h4>
-          <h5> Item Type: {product.itemType} </h5>
+        <div key = {item.id}>
+          <img src = {item.image} />
+          <h4> Name: {item.name}</h4>
+          <h6> Description: {item.description} </h6>
+          <h4> Price: {item.price} </h4>
+          <h5> Item Type: {item.itemType} </h5>
+          {/* <Edit handleUpdate={handleUpdate} item={item} /> */}
+          <button onClick={() => {handleDelete(item)}} value={item.id}>Delete</button>
         </div>
       )
     })}
-
+    </div>
     </>
   )
 }
