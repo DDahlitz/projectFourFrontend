2200d01ff004b110c5f43a9e50b8718ae2eaed25
Miguel Castaneda <miguelcastaneda@Miguels-MBP-3.lan>
2022-09-14 06:49:52 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-
+import {useEffect, useState} from 'react'
+import axios from 'axios'
 
 
 const App = () => {
-  
+  const [users, setUsers] = useState([])
+  const [products, setProducts] = useState([])
+
 
   return (
     <h1>Header</h1>
   )
 }
+
+export default App;
