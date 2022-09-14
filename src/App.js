0605e28b2aeae6ec3897274bbd5eabd93ac4627f
Derrick Dahlitz <ddahlitz@iMac-28.lan>
2022-09-14 07:36:07 -0700

    removing commented out sections of app.js

--- src/App.js
+++ src/App.js
@@ -63,7 +63,7 @@ useEffect(() => {
           <h6> Description: {item.description} </h6>
           <h4> Price: {item.price} </h4>
           <h5> Item Type: {item.itemType} </h5>
-          {/* <Edit handleUpdate={handleUpdate} item={item} /> */}
+          <Edit handleUpdate={handleUpdate} item={item} />
           <button onClick={() => {handleDelete(item)}} value={item.id}>Delete</button>
         </div>
       )
