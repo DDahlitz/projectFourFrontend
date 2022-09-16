713488a6296231c6e6eb8c7a7be137aacb4e9a3b
romantafelski <romantafelski18@gmail.com>
2022-09-16 10:25:56 -0400

    jds;lf

--- src/App.js
+++ src/App.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-import "./App.css";
-import { useState, useEffect, isValidElement } from "react";
-import React from "react";
-import axios from "axios";
-=======
 import './App.css'
 import {useState, useEffect, isValidElement} from 'react'
 import React from 'react'
@@ -14,93 +8,20 @@ import New from './components/New'
 
 import Login from './components/Login'
 import AddUser from './components/AddUser'
->>>>>>> f96f591260d1e7cbd4f62132a47e5189dba8060d
 
-import Edit from "./components/Edit";
-import New from "./components/New";
-import Login from "./components/Login";
-import AddUser from "./components/AddUser";
 
 
 const App = () => {
-  const [products, setProducts] = useState([]);
-  const [loginHeader, setLoginHeader] = useState(false);
-  const [displayLogin, setDisplayLogin] = useState(false);
+  const [products, setProducts] = useState([])
+  const [loginHeader, setLoginHeader] = useState(false)
+  const [displayLogin, setDisplayLogin] = useState(false)
   const [show, setShow] = useState(false);
-  const [loginSuccess, setLoginSuccess] = useState(false);
-  const [showProduct, setShowProduct] = useState(false);
-  const [user, setUser] = useState([]);
-  const [currentUser, setCurrentUser] = useState([]);
-  const [newUser, setNewUser] = useState([]);
+  const [loginSuccess, setLoginSuccess] = useState(false)
+  const [showProduct, setShowProduct] = useState(false)
+  const [user, setUser] = useState([])
+  const [currentUser, setCurrentUser] = useState([])
+  const [newUser, setNewUser] = useState([])
 
-<<<<<<< HEAD
-  const getProducts = () => {
-    axios
-      .get("http://localhost:8000/api/products")
-      .then(
-        (response) => setProducts(response.data),
-        (err) => console.error(err)
-      )
-      .catch((error) => console.error(error));
-  };
-
-  const handleCreate = (addItem) => {
-    let nextId = products[products.length - 1].id + 1;
-    axios
-      .post("http://localhost:8000/api/products", addItem)
-      .then((response) => {
-        addItem.id = nextId;
-        setProducts([...products, response.data]);
-      });
-  };
-
-  const handleDelete = (deletedItem) => {
-    axios
-      .delete("http://localhost:8000/api/products/" + deletedItem.id)
-      .then((response) => {
-        setProducts(products.filter((item) => item.id !== deletedItem.id));
-      });
-  };
-
-  const handleUpdate = (editItem) => {
-    axios
-      .put("http://localhost:8000/api/products/" + editItem.id, editItem)
-      .then((response) => {
-        setProducts(
-          products.map((item) => {
-            return item.id !== editItem.id ? item : editItem;
-          })
-        );
-      });
-  };
-
-  const handleLogin = (findUser) => {
-    axios
-      .put("http://localhost:8000/api/useraccount/login", findUser)
-      .then((response) => {
-        if (response.data.email == null) {
-          alert("Email and Password Do Not Match");
-        } else {
-          setUser(response.data);
-          axios
-            .get("http://localhost:8000/api/useraccount/" + response.data.id)
-            .then((response) => {
-              setCurrentUser(response.data);
-            });
-          setShowProduct(true);
-          setLoginSuccess(false);
-          console.log(response.data);
-        }
-      });
-  };
-
-  const handleNewUser = (addUser) => {
-    axios
-      .post("http://localhost:8000/api/useraccount", addUser)
-      .then((response) => {
-        setNewUser([...newUser, response.data], (err) => console.error(err));
-        alert("ACCOUNT CREATED, NOW LOGIN");
-=======
 
 
 
@@ -157,23 +78,14 @@ const handleLogin = (findUser) => {
       axios
       .get('http://localhost:8000/api/useraccount/' + response.data.id).then((response) => {
         setCurrentUser(response.data)
->>>>>>> f96f591260d1e7cbd4f62132a47e5189dba8060d
       })
-      .catch((error) => alert("Username already taken"));
-  };
+    setShowProduct(true)
+    setLoginSuccess(false)
+    console.log(response.data)
+    }
+  })
+}
 
-<<<<<<< HEAD
-  useEffect(() => {
-    getProducts();
-  }, []);
-
-  const showPage = () => {
-    setLoginHeader(false);
-    setDisplayLogin(false);
-    setShow(true);
-    setShowProduct(false);
-  };
-=======
 // ======== DELETE USER AND ALL ASSOCIATED PRODUCTS =======
 
 const handleDeleteUser = () => {
@@ -207,34 +119,21 @@ const handleNewUser = (addUser) => {
 useEffect(() => {
   getProducts()
 }, []) 
->>>>>>> f96f591260d1e7cbd4f62132a47e5189dba8060d
 
-  const showloginAndHideCreate = () => {
-    setDisplayLogin(true);
-    setShow(false);
-    setLoginHeader(true);
-    setLoginSuccess(true);
-  };
+const showPage = () => {
+  setLoginHeader(false)
+  setDisplayLogin(false)
+  setShow(true)
+  setShowProduct(false)
+}
 
-  const showProductInput = () => {
-    setShowProduct(true);
-  };
+const showloginAndHideCreate = () => {
+  setDisplayLogin(true)
+  setShow(false)
+  setLoginHeader(true)
+  setLoginSuccess(true)
+}
 
-<<<<<<< HEAD
-  const goBack = () => {
-    setLoginHeader(false);
-    setDisplayLogin(false);
-    setLoginSuccess(false);
-  };
-
-  const logout = () => {
-    setUser([]);
-    setCurrentUser([]);
-    setShowProduct(false);
-    setDisplayLogin(false);
-    setLoginHeader(false);
-  };
-=======
 const goBack =() => {
   setLoginHeader(false)
   setDisplayLogin(false)
@@ -248,53 +147,20 @@ const logout = () => {
   setDisplayLogin(false)
   setLoginHeader(false)
 }
->>>>>>> 411fac53c81f381c395931829192a6e96315862b
 
 
   return (
     <>
-      <div className="container">
-        {loginHeader ? null : (
-          <div>
-            <h1>TECHY</h1>
-            <h2>CREATE AN ACCOUNT</h2>
-          </div>
-        )}
-        {displayLogin ? null : (
-          <button className="button" onClick={showPage}>
-            Create Account
-          </button>
-        )}
-        {displayLogin ? null : (
-          <button
-            className="button"
-            onClick={() => {
-              showloginAndHideCreate();
-            }}
-          >
-            Login
-          </button>
-        )}
-        {showProduct ? (
-          <>
-            <button className="button-primary" onClick={logout}>
-              Log Out
-            </button>
-          </>
-        ) : null}
+      <div className = 'container'>
+        {loginHeader ? null : <div><h1>TECHY</h1><h2>CREATE AN ACCOUNT</h2></div>}
+        {displayLogin ? null : <button className = 'button' onClick={showPage}>Create Account</button>}
+        {displayLogin ? null : <button className='button' onClick={() => {
+                showloginAndHideCreate()
+              }}>Login</button>}
+        {showProduct ? <><button className="button-primary" onClick={logout}>Log Out</button>
+        </> : null}
       </div>
       <div className="container">
-<<<<<<< HEAD
-        {displayLogin ? <h2>YOUR TECHY PAGE</h2> : null}
-        {showProduct ? <h4>Welcome to Techy, {user.email}!</h4> : null}
-        {loginSuccess ? <h5>Log In Here</h5> : null}
-      </div>
-      <div className="loginForm">
-        {show ? (
-          <div>
-            <AddUser handleNewUser={handleNewUser} />
-            <hr />
-=======
               {displayLogin ? <h2>YOUR TECHY PAGE</h2> : null}
               {showProduct ? <h4>Welcome to Techy, {currentUser.email}!</h4> : null}
               {loginSuccess ? <h5>Log In Here</h5> : null}
@@ -336,56 +202,10 @@ const logout = () => {
               <button onClick={() => {handleDelete(item)}} value={item.id}>Delete</button>
             </div>
           )
-<<<<<<< HEAD
-        })} */}
->>>>>>> 411fac53c81f381c395931829192a6e96315862b
-=======
         })}
->>>>>>> f96f591260d1e7cbd4f62132a47e5189dba8060d
           </div>
-        ) : null}
-        {loginSuccess ? (
-          <Login
-            handleLogin={handleLogin}
-            loginSuccess={loginSuccess}
-            goBack={goBack}
-          />
-        ) : null}
-        {showProduct ? (
-          <div>
-            <h4>Add a New Product</h4>
-            <New user={user} handleCreate={handleCreate} />
-          </div>
-        ) : null}
-        {products
-          .filter((potato) => {
-            if (potato.email !== user.email) {
-              return potato;
-            }
-          })
-          .map((item) => {
-            return (
-              <div key={item.id}>
-                <h4> Name: {item.name}</h4>
-                <img src={item.image} />
-                <h6> Description: {item.description} </h6>
-                <h4> Price: {item.price} </h4>
-                <h5> Item Type: {item.itemType} </h5>
-                <Edit handleUpdate={handleUpdate} item={item} />
-                <button
-                  onClick={() => {
-                    handleDelete(item);
-                  }}
-                  value={item.id}
-                >
-                  Delete
-                </button>
-              </div>
-            );
-          })}
-      </div>
     </>
-  );
-};
+  )
+}
 
 export default App;
--- src/components/New.js
+++ src/components/New.js
@@ -1,47 +1,47 @@
-import {useState} from 'react'
+import { useState } from 'react'
 
 const New = (props) => {
-    let emptyItem = {name: '', image: '', description: '', price: '', itemType: '' }
+    let emptyItem = { name: '', image: '', description: '', price: '', itemType: '' }
     const [item, setItem] = useState(emptyItem)
 
     const handleChange = (event) => {
-        setItem({...item, [event.target.name]: event.target.value})
+        setItem({ ...item, [event.target.name]: event.target.value })
     }
 
     const handleSubmit = (event) => {
         event.preventDefault()
         props.handleCreate(item)
         item.useraccount = props.user.id
-        setItem({name: '', image: '', description: '', price: '', itemType: '', useraccount:'' })
+        setItem({ name: '', image: '', description: '', price: '', itemType: '', useraccount: '' })
     }
 
     return (
         <>
-        <form className="newForm" onSubmit={handleSubmit}>
-            <div className='hidden'>
-                {item.useraccount = props.user.id}
-            </div>
-            <label htmlFor="name">Name: </label>
-            <input className='newBox' type="text" name="name" value={item.name} onChange={handleChange} />
-            <br />
-            <br />
-            <label htmlFor="image">Image: </label>
-            <input className='newBox' type="text" name="image" value={item.image} onChange={handleChange} />
-            <br />
-            <br />
-            <label htmlFor="description">Description: </label>
-            <input className='newBox' type="text" name="description" value={item.description} onChange={handleChange} />
-            <br />
-            <br />
-            <label htmlFor="price">Price: </label>
-            <input className='newBox' type="number" name="price" value={item.price} onChange={handleChange} />
-            <br />
-            <br />
-            <label htmlFor="itemType">Item Type: </label>
-            <input className='newBox' type="text" name="itemType" value={item.itemType} onChange={handleChange}/>
-            <input className='newButton' type="submit"/>
-      </form>
-    </>
+            <form className="newForm" onSubmit={handleSubmit}>
+                <div className='hidden'>
+                    {item.useraccount = props.user.id}
+                </div>
+                <label htmlFor="name">Name: </label>
+                <input className='newBox' type="text" name="name" value={item.name} onChange={handleChange} />
+                <br />
+                <br />
+                <label htmlFor="image">Image: </label>
+                <input className='newBox' type="text" name="image" value={item.image} onChange={handleChange} />
+                <br />
+                <br />
+                <label htmlFor="description">Description: </label>
+                <input className='newBox' type="text" name="description" value={item.description} onChange={handleChange} />
+                <br />
+                <br />
+                <label htmlFor="price">Price: </label>
+                <input className='newBox' type="number" name="price" value={item.price} onChange={handleChange} />
+                <br />
+                <br />
+                <label htmlFor="itemType">Item Type: </label>
+                <input className='newBox' type="text" name="itemType" value={item.itemType} onChange={handleChange} />
+                <input className='newButton' type="submit" />
+            </form>
+        </>
     )
 }
 
